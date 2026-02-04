29acae1441be132d9c202cfd81699f084f11d6ca
Prashanth2025 <prashanthramesh.02@gmail.com>
2026-02-04 15:18:59 +0530

    reload fix 3

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-     viteStaticCopy({
+  plugins: [
+    react(),
+    viteStaticCopy({
       targets: [
         {
-          src: 'public/_redirects', // source file
-          dest: '.'                  // copy to root of dist
-        }
-      ]
-    })
+          src: "public/_redirects", // ✅ correct relative path
+          dest: ".", // copy to dist root
+        },
+      ],
+    }),
   ],
-})
+});
