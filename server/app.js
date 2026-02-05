d1bcf56f5ce5b5a2c30ee1062c00a82b38c8344a
Prashanth2025 <prashanthramesh.02@gmail.com>
2026-02-05 11:18:52 +0530

    async

--- server/app.js
+++ server/app.js
@@ -12,7 +12,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 app.use(cors());
